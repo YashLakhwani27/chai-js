fdebff8da65cfd20ab9be7479cdf2051af262001
YashLakhwani27 <yashlakhwani010@gmail.com>
2025-06-05 08:59:34 +0000

    Strings , Numbers , Math

---
+++ 01_basics/Strings.js
@@ -0,0 +1,78 @@
+const name = "Yash"
+const repoCount = 50
+
+// console.log(`Hello my name is ${name} and repoCount is ${repoCount}`);
+
+let gameName = new String('YashLakhwani')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(-12,1));
+
+
+let newName = "              Yash Lakhwani       "
+// console.log(newName.trim());
+
+const url = "https://yash.com//yashlakhwani%20lakhwani"
+// console.log(url);
+
+// console.log(url.replace('%' , '-'));
+// console.log(url.includes('yash'));
+// console.log(url.split('%'));
+
+
+
+
+// ***********************************************************
+
+// ********************** Numbers and Maths ************************************
+
+const score = 400
+
+let num = new Number(100)
+// console.log(num);
+
+// console.log(num.toFixed(2))
+// console.log(num.toString().length)
+
+const otherNumber = 123.2344
+// console.log(otherNumber.toPrecision(3)); // starts from begining 
+// console.log(otherNumber.toPrecision(4)); 
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********** Maths ****************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.2));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.max(4,6,2,8));
+console.log(Math.min(4,6,2,8));
+
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 + 1)); // *10 for moving decimal one digit and + 1 for handle 0.0 digits
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1) + min));
+
+
+ 
+
+
+
+
+
+
+
