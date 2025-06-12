3e4e12917b0907fdbeb5a246a584f701e82f4a98
YashLakhwani27 <yashlakhwani010@gmail.com>
2025-06-12 15:40:39 +0000

    Nullish Coalescing Operator(??) , Terniary Operator , truty and falsy values

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,69 @@
+// if(2 == "2"){
+//     console.log("Executed");
+    
+// }
+// if(2 === "2"){ // Not Executed datatype is different
+//     console.log("Executed Too");
+    
+// }
+
+// const score = 100
+
+// if(score > 80){
+//     // var power = "fly" // Scope issue
+//     let power = "fly" 
+//     console.log("Power: ",power);
+    
+// }
+
+// // console.log("Power: ",power);
+
+
+// switch case
+
+// const month = 4
+
+// switch (month) {
+//     case 1:
+//         console.log("Janurary");
+//         break;
+//     case 2:
+//         console.log("Febuary");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+
+//     default:
+//         console.log("match not found");
+        
+//         break;
+// }
+
---
+++ 04_control_flow/truty_falsy_values.js
@@ -0,0 +1,64 @@
+// const userEmail = "yash@ai.com"
+const userEmail = []
+
+// if(userEmail){
+//     console.log("Got the user Email");
+// }
+// else{
+//     console.log("Not found the user Email");
+// }
+
+// falsy values
+// false , 0 , -0 , BigInt => 0n ,null , undefined , NaN , ""
+
+// truty values
+// "0" , "yash", true , 'false' , " " , function(){} , [] , {}
+
+// if("0"){
+//     console.log("Executed");
+// }
+
+// if ('false') {
+//     console.log("Executed");
+// }
+
+// if (" ") {
+//     console.log("Executed");
+// }
+
+// if (userEmail.length === 0){
+//     console.log("Array is Empty");
+    
+// }
+
+const obj = {}
+
+// if (Object.keys(obj).length === 0) {
+//     console.log("Object is Empty");
+// }
+
+// Nullish Coalescing Operator (??)  : null ,undefined
+
+// val1 = 5 ?? 10 // gives first value
+// console.log(val1);
+
+// val1 = null ?? 5
+// val1 = 5 ?? null
+
+// val1 = 10 ?? undefined
+
+// val1 = undefined ?? 10 ?? 20 // gives the first value 
+// val1 = 10 ?? undefined ?? 20
+// val1 = 10 ?? null ?? 20
+// console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("More than 80") : console.log("Less than 80");
+iceTeaPrice >= 80 ? console.log("More than 80") : console.log("Less than 80");
+
+
+
