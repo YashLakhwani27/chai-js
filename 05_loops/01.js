52a783686c06e30acbe1c35f29b33a1a700028bd
YashLakhwani27 <yashlakhwani010@gmail.com>
2025-06-14 15:25:12 +0000

    for , while,do-while

---
+++ 05_loops/01.js
@@ -0,0 +1,53 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value is ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//         console.log(i ," * " , j ," = ",i*j);
+        
+//     }
+    
+// }
+
+
+    // for (let i = 0; i <= 20; i++) {
+
+    //     if (i == 5) {
+    //         console.log("5 is best number");
+    //         break
+            
+    //     }
+    //     console.log(`Outer value is ${i}`);
+        
+    // }
+
+    // for (let i = 0; i <= 20; i++) {
+
+    //     if (i == 5) {
+    //         console.log("5 is best number");
+    //         continue
+            
+    //     }
+    //     console.log(`Outer value is ${i}`);
+        
+    // }
+
+
+    // while loop
+
+const myArr = ["flash","superman","batman"]
+let arr = 0;
+
+while(arr < myArr.length){
+    console.log(myArr[arr]);
+    arr++;
+}
+
+
+let score = 1;
+
+do{
+    console.log(score);
+    score++;
+    
+}while(score <= 10)
